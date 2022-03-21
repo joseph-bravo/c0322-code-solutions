c88f0b03295381915e56dad5ff64d8d70e3df697
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-21 21:57:17 +0000

    completed: javascript-objects

---
+++ javascript-objects/main.js
@@ -0,0 +1,14 @@
+var student = {
+  firstName: 'Joseph',
+  lastName: 'Bravo',
+  age: 20
+};
+
+var fullName = student.firstName + ' ' + student.lastName;
+
+console.log('value of fullName:', fullName);
+
+student.livesInIrvine = true;
+student.previousOccupation = 'retail worker';
+console.log('lives in irvine?: ', student.livesInIrvine);
+console.log('previous occupation?: ', student.livesInIrvine);
