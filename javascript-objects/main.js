0a54cb3a0d6eb60739eddae754c049d3fe5c894d
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-21 22:15:12 +0000

    revisions made

--- javascript-objects/main.js
+++ javascript-objects/main.js
@@ -11,4 +11,27 @@ console.log('value of fullName:', fullName);
 student.livesInIrvine = true;
 student.previousOccupation = 'retail worker';
 console.log('lives in irvine?: ', student.livesInIrvine);
-console.log('previous occupation?: ', student.livesInIrvine);
+console.log('previous occupation?: ', student.previousOccupation);
+console.log('value of student', student);
+
+var vehicle = {
+  make: 'BMW',
+  model: 'cool car',
+  year: 2022
+};
+
+vehicle.color = 'blue';
+vehicle.isConvertible = true;
+console.log('vehicle color?:', vehicle.color);
+console.log('vehicle is convertible?:', vehicle.isConvertible);
+console.log('vehicle object:', vehicle);
+
+var pet = {
+  name: 'birch',
+  type: 'cat'
+};
+
+delete pet.name;
+delete pet.type;
+
+console.log('value of pet', pet);
