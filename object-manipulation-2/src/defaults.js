066e4626db9f33f20dcd3f9e048b790ff79463d7
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-25 23:52:21 +0000

    completed: object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,18 @@
 /* exported defaults */
+/* global omit */
+/*
+new output empty object
+grab target keys into an array of strings Object.toKeys()
+use omit(), source as 1st arg and targetKeys as 2nd arg, assign to var
+for each value in source remaining and all of target, add into output
+return output
+ */
+
+function defaults(target, source) {
+  var targetKeys = Object.keys(target);
+  var sourceFiltered = omit(source, targetKeys);
+
+  for (var key2 in sourceFiltered) {
+    target[key2] = source[key2];
+  }
+}
--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,13 @@
 /* exported invert */
+/*
+empty object for output
+for each property (for...in), set value as property and key as value to new object
+ */
+
+function invert(source) {
+  var output = {};
+  for (var key in source) {
+    output[source[key]] = key;
+  }
+  return output;
+}
