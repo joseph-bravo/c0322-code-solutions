fcfbd700111be0f08ec66bde0d136fa9dcca6d87
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-26 00:36:09 +0000

    revised omit.js with for...in loop

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -7,19 +7,39 @@ variable: unbanned empty array, list out source keys,
 once that's done, iterate through unbanned array and add property to obj output
 */
 
+// function omit(source, argKeys) {
+//   var output = {};
+//   var sourceKeys = Object.keys(source);
+//   var unbannedKeys = [];
+//   for (var i = 0; i < sourceKeys.length; i++) {
+//     var isOkay = true;
+//     for (var x = 0; x < argKeys.length; x++) {
+//       if (sourceKeys[i] === argKeys[x]) {
+//         isOkay = false;
+//       }
+//     }
+//     if (isOkay) {
+//       unbannedKeys.push(sourceKeys[i]);
+//     }
+//   }
+//   for (var i = 0; i < unbannedKeys.length; i++) {
+//     output[unbannedKeys[i]] = source[unbannedKeys[i]];
+//   }
+//   return output;
+// }
+
 function omit(source, argKeys) {
   var output = {};
-  var sourceKeys = Object.keys(source);
   var unbannedKeys = [];
-  for (var i = 0; i < sourceKeys.length; i++) {
+  for (var i in source) {
     var isOkay = true;
     for (var x = 0; x < argKeys.length; x++) {
-      if (sourceKeys[i] === argKeys[x]) {
+      if (i === argKeys[x]) {
         isOkay = false;
       }
     }
     if (isOkay) {
-      unbannedKeys.push(sourceKeys[i]);
+      unbannedKeys.push(i);
     }
   }
   for (var i = 0; i < unbannedKeys.length; i++) {
