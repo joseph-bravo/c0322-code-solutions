2bfdfd74c915521099c8eeb30d6b2a8e9331c2f0
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-26 00:32:43 +0000

    revised with math.max method

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -2,16 +2,14 @@
 
 /*
 loop through array starting at given length - index,
-  check if i is within range of indexes (greater than or equal 0)
+  use Math.max to set i to 0 if array.length - count is less than 0
 push into output array then return that
 */
 
 function takeRight(array, count) {
   var output = [];
-  for (var i = array.length - count; i < array.length; i++) {
-    if (i >= 0) {
-      output.push(array[i]);
-    }
+  for (var i = Math.max(0, array.length - count); i < array.length; i++) {
+    output.push(array[i]);
   }
   return output;
 }
