100723830834306aa50377668cdb92be1f624c71
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-29 22:46:42 +0000

    optional enhancements implemented + some extras

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,28 +1,76 @@
-var $letterNodes = document.querySelectorAll('.phrase h1 span');
-var $completionText = document.querySelector('.completion-text');
-var currentLetter = 0;
-var letters = [];
+var $phraseH1 = document.querySelector('.phrase h1');
+var $letterNodes = $phraseH1.children;
 
-for (var i = 0; i < $letterNodes.length; i++) {
-  letters.push($letterNodes[i].textContent);
+var phrase;
+var phraseList = [
+  "i don't know how to handle capital letters lol",
+  'top 10 ways to code',
+  'bababooey bababooey bababooey',
+  'optional keyboard stuff',
+  'typing random things',
+  'asdf jkl;',
+  'jfjfjfjf',
+  'me when the uhhhhhh',
+  'alphabet',
+  'cross country',
+  'new zealand',
+  'learning fuze'
+];
+
+var currentLetter = 0;
+
+var $completionText = document.querySelector('.completion-text');
+var $accuracyText = document.querySelector('.accuracy');
+var missCount = 0;
+var hitCount = 0;
+
+function reset() {
+  currentLetter = 0;
+  $completionText.className = 'row completion-text hidden';
+  while ($letterNodes.length > 0) {
+    $letterNodes[0].remove();
+  }
+}
+
+function initializePhrase() {
+  reset();
+
+  phrase = phraseList[Math.floor(phraseList.length * (Math.random()))];
+  for (var i = 0; i < phrase.length; i++) {
+    var letterNode = document.createElement('span');
+    letterNode.appendChild(document.createTextNode(phrase[i]));
+    $phraseH1.append(letterNode);
+  }
+  $letterNodes[currentLetter].className = 'current-letter';
+}
+
+function winState() {
+  var accuracyNoDecimal = Math.round(((hitCount / (hitCount + missCount)) * 100));
+  $accuracyText.textContent = accuracyNoDecimal + '%';
+  $completionText.className = 'row completion-text';
 }
 
 function checkInput(event) {
-  if (currentLetter < letters.length) {
-    if (event.key === letters[currentLetter]) {
+  if (currentLetter < phrase.length) {
+    if (event.key === phrase[currentLetter]) {
       $letterNodes[currentLetter].className = 'correct-char';
       currentLetter++;
-      if (currentLetter < letters.length) {
+      hitCount++;
+      if (currentLetter < phrase.length) {
         $letterNodes[currentLetter].className = 'current-letter';
       } else {
-        $completionText.className = 'row completion-text';
+        winState();
       }
     } else {
       $letterNodes[currentLetter].className = 'current-letter incorrect-char';
+      missCount++;
     }
+  } else if (event.key === 'Enter') {
+    initializePhrase();
   }
 }
 
 var $window = document.defaultView;
-
 $window.addEventListener('keydown', checkInput);
+
+initializePhrase();
