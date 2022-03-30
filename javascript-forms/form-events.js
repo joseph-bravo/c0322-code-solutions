3f17e20af297090ae63ded4270bc16b9c195c8cc
Joseph Bravo <josephangelobravo@gmail.com>
2022-03-30 18:40:06 +0000

    revised javascript-forms, separated form-events and form-values

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -12,28 +12,22 @@ function handleInput(event) {
   console.log('handle input: name:', event.target.name, '|| value:', event.target.value);
 }
 
+// grab everything
 var $contactForm = document.forms['contact-form'];
 var $contactName = $contactForm.name;
 var $contactEmail = $contactForm.email;
 var $contactMessage = $contactForm.message;
+
+// for the loop
 var eventListenersToAdd = ['focus', 'blur', 'input'];
 var eventHandlers = [handleFocus, handleBlur, handleInput];
 
+// stuff we wanna add listeners to
 var contactValues = [$contactName, $contactEmail, $contactMessage];
 
+// add listener to all in contact values array
 for (var i = 0; i < contactValues.length; i++) {
   for (var e = 0; e < eventListenersToAdd.length; e++) {
     contactValues[i].addEventListener(eventListenersToAdd[e], eventHandlers[e]);
   }
 }
-
-$contactForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  var messageData = {};
-  for (var i = 0; i < contactValues.length; i++) {
-    messageData[contactValues[i].name] = contactValues[i].value;
-  }
-  console.log(messageData);
-  $contactForm.reset();
-});
