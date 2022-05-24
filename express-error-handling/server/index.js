56d350c5684f22188f519f6e1660ac1357973cb7
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-24 19:41:29 +0000

    fixed 404 errors

--- express-error-handling/server/index.js
+++ express-error-handling/server/index.js
@@ -68,9 +68,7 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
     .then(result => {
       const [grade] = result.rows;
       if (!grade) {
-        res.status(404).json({
-          error: `cannot find grade with gradeId ${gradeId}`
-        });
+        throw new ClientError(404, `cannot find grade with gradeId ${gradeId}`);
       } else {
         res.json(grade);
       }
@@ -104,9 +102,7 @@ app.put('/api/grades/:gradeId', (req, res, next) => {
     .then(result => {
       const [updatedGrade] = result.rows;
       if (!updatedGrade) {
-        res.status(404).json({
-          error: `cannot find grade with gradeId ${gradeId}`
-        });
+        throw new ClientError(404, `cannot find grade with gradeId ${gradeId}`);
       } else {
         res.json(updatedGrade);
       }
@@ -129,9 +125,7 @@ app.delete('/api/grades/:gradeId', (req, res, next) => {
     .then(result => {
       const [deletedGrade] = result.rows;
       if (!deletedGrade) {
-        res.status(404).json({
-          error: `cannot find grade with gradeId ${gradeId}`
-        });
+        throw new ClientError(404, `cannot find grade with gradeId ${gradeId}`);
       } else {
         res.sendStatus(204);
       }
